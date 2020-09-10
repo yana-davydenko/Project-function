df7e823dc97b724d35c5b3451419b1439cf8047a
iana-davydenko <iana.lady1302@gmail.com>
2020-09-10 22:10:49 +0300

    Initial commit

---
+++ main4.py
@@ -0,0 +1,11 @@
+def function(a, b, c):
+    if (a + b) > c:
+        print ('greater than c')
+    elif (a + b) <= c:
+        print ('less than c')
+
+function(8, 15, 100)
+function(300, 20, 10)
+function(5, 5, 10)
+function(15, 25, 10)
+
